cdd5cd9801b52ce800a59913003a34bc02d40e5b
pavlo.yutish <od210291jpv@gmail.com>
2026-07-10 22:35:55 +0300

    updated infra files

--- src/ContentCms.API/Program.cs
+++ src/ContentCms.API/Program.cs
@@ -84,11 +84,8 @@ builder.Services.AddScoped<IContentService, ContentService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Content CMS API"));
-}
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Content CMS API"));
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 
